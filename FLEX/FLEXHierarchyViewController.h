7062795c879d78f0a40a78994ecefe8b0e240820
pxx917144686 <111606963+pxx917144686@users.noreply.github.com>
2025-05-06 18:25:55 +0800

    Add files via upload

---
+++ FLEX/FLEXHierarchyViewController.h
@@ -0,0 +1,29 @@
+//
+//  FLEXHierarchyViewController.h
+//  FLEX
+//
+//  Created by Tanner Bennett on 1/9/20.
+//  Copyright © 2020 FLEX Team. All rights reserved.
+//
+// 遇到问题联系中文翻译作者：pxx917144686
+
+#import "FLEXNavigationController.h"
+
+@protocol FLEXHierarchyDelegate <NSObject>
+- (void)viewHierarchyDidDismiss:(UIView *)selectedView;
+@end
+
+/// A navigation controller which manages two child view controllers:
+/// a 3D Reveal-like hierarchy explorer, and a 2D tree-list hierarchy explorer.
+@interface FLEXHierarchyViewController : FLEXNavigationController
+
++ (instancetype)delegate:(id<FLEXHierarchyDelegate>)delegate;
++ (instancetype)delegate:(id<FLEXHierarchyDelegate>)delegate
+              viewsAtTap:(NSArray<UIView *> *)viewsAtTap
+            selectedView:(UIView *)selectedView;
+
+- (void)toggleHierarchyMode;
+
+@property (nonatomic, readonly) UIView *selectedView;
+
+@end
